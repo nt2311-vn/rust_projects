9fbdcf35be729ab6bc84e9009e9d2cea75e62cbf
nt2311-vn <nguyentoan231196@gmail.com>
2025-06-01 16:09:44 +0700

    cargo run project

---
+++ catsay/target/debug/catsay.d
@@ -0,0 +1,1 @@
+/home/nt2311-vn/Projects/Rust/rust_projects/catsay/target/debug/catsay: /home/nt2311-vn/Projects/Rust/rust_projects/catsay/src/main.rs
---
+++ catsay/target/debug/deps/catsay-1dae36f72ad69d92.d
@@ -0,0 +1,5 @@
+/home/nt2311-vn/Projects/Rust/rust_projects/catsay/target/debug/deps/catsay-1dae36f72ad69d92: src/main.rs
+
+/home/nt2311-vn/Projects/Rust/rust_projects/catsay/target/debug/deps/catsay-1dae36f72ad69d92.d: src/main.rs
+
+src/main.rs:
