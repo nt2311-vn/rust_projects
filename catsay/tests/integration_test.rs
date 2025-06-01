734300644f4a772a67cbedee7670db638c0b31ed
nt2311-vn <nguyentoan231196@gmail.com>
2025-06-01 22:02:00 +0700

    write test when provide a file not exists ensure failure on running

--- catsay/tests/integration_test.rs
+++ catsay/tests/integration_test.rs
@@ -1,6 +1,6 @@
 use assert_cmd::prelude::*;
 use predicates::prelude::*;
-use std::process::Command;
+use std::{process::Command, result::Result};
 
 #[test]
 fn run_with_defaults() {
@@ -9,4 +9,15 @@ fn run_with_defaults() {
         .assert()
         .success()
         .stdout(predicate::str::contains("Meow!"));
+}
+
+#[test]
+fn fail_on_non_existing_file() -> Result<(), Box<dyn std::error::Error>> {
+    Command::cargo_bin("catsay")
+        .expect("binary exists")
+        .args(&["-f", "no/such/file.txt"])
+        .assert()
+        .failure();
+
+    Ok(())
 }
