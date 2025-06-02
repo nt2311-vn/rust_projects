1ce1d1271341857a9983396cf2ed42ef9ee1e23c
nt2311-vn <nguyentoan231196@gmail.com>
2025-06-02 22:02:02 +0700

    write input step to declare message and isdead to the TUI program

--- catsay-tui/src/main.rs
+++ catsay-tui/src/main.rs
@@ -1,4 +1,14 @@
-use cursive::{event::Key, views::Dialog};
+use cursive::{
+    event::Key,
+    view::Nameable,
+    views::{Checkbox, Dialog, EditView, ListView},
+    Cursive,
+};
+
+struct CatsayOptions<'a> {
+    message: &'a str,
+    dead: bool,
+}
 
 fn main() {
     let mut siv = cursive::default();
@@ -14,4 +24,30 @@ fn main() {
     // Listen to Key::Esc and quit
     siv.add_global_callback(Key::Esc, |s| s.quit());
     siv.run();
+}
+
+fn input_step(siv: &mut Cursive) {
+    siv.add_layer(
+        Dialog::new()
+            .title("Please fill out the form for the xcat")
+            .content(
+                ListView::new()
+                    .child("Message:", EditView::new().with_name("message"))
+                    .child("Dead?", Checkbox::new().with_name("dead")),
+            )
+            .button("OK", |s| {
+                let message = s
+                    .call_on_name("message", |t: &mut EditView| t.get_content())
+                    .unwrap();
+
+                let is_dead = s
+                    .call_on_name("dead", |t: &mut Checkbox| t.is_checked())
+                    .unwrap();
+
+                let options = CatsayOptions {
+                    message: &message,
+                    dead: is_dead,
+                };
+            }),
+    );
 }
