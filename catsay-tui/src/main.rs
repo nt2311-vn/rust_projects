431f5daf8f3af5e800738bf15c3ccc0782addba1
nt2311-vn <nguyentoan231196@gmail.com>
2025-06-02 20:45:23 +0700

    add a layout to the tui program and use TextView to create layout as ASCII

--- catsay-tui/src/main.rs
+++ catsay-tui/src/main.rs
@@ -1,4 +1,14 @@
+use cursive::views::TextView;
+
 fn main() {
     let mut siv = cursive::default();
+    let cat_text = "Meow!
+\\
+ \\
+    /\\_/\\
+   ( O O )
+   =( I )=";
+
+    siv.add_layer(TextView::new(cat_text));
     siv.run();
 }
