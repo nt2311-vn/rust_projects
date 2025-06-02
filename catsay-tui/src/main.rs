7b2a1bdb995e2676dc3be059c016d0a188f282fb
nt2311-vn <nguyentoan231196@gmail.com>
2025-06-02 22:11:00 +0700

    readd the global callback to end the program when press ESC

--- catsay-tui/src/main.rs
+++ catsay-tui/src/main.rs
@@ -12,16 +12,7 @@ struct CatsayOptions<'a> {
 
 fn main() {
     let mut siv = cursive::default();
-    let cat_text = "Meow!
-\\
- \\
-    /\\_/\\
-   ( O O )
-   =( I )=";
-
-    siv.add_layer(Dialog::text(cat_text).button("OK", |s| s.quit()));
-
-    // Listen to Key::Esc and quit
+    input_step(&mut siv);
     siv.add_global_callback(Key::Esc, |s| s.quit());
     siv.run();
 }
