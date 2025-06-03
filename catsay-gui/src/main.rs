71b2c1807d42bf604b1a8ef226a201b946751529
nt2311-vn <nguyentoan231196@gmail.com>
2025-06-03 21:04:14 +0700

    compile project with images cat pnfg

--- catsay-gui/src/main.rs
+++ catsay-gui/src/main.rs
@@ -1,5 +1,7 @@
+use std::path::Path;
+
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow};
+use gtk::{Application, ApplicationWindow, Box as GtkBox, Image, Label, Orientation};
 
 fn main() {
     let app = Application::new(Some("nt2311-vn.catsay-gui"), Default::default());
@@ -8,6 +10,19 @@ fn main() {
         let window = ApplicationWindow::new(app);
         window.set_title("Catsay");
         window.set_default_size(350, 70);
+        let layout_box = GtkBox::new(Orientation::Vertical, 0);
+        let label = Label::new(Some("Meow!\n    \\\n    \\"));
+        layout_box.add(&label);
+
+        let image_path = "./images/cat.png";
+        if Path::new(image_path).exists() {
+            let cat_image = Image::from_file(image_path);
+            layout_box.add(&cat_image);
+        } else {
+            let error_label = Label::new(Some("Image not found!"));
+            layout_box.add(&error_label);
+        }
+        window.add(&layout_box);
         window.show_all();
     });
 
