e8b43882e4eec3d761771b2fb6eab248b4d1a260
nt2311-vn <nguyentoan231196@gmail.com>
2025-06-03 21:53:56 +0700

    create a new catsay to develop glade UI

---
+++ catsay-gui-glade/target/debug/deps/catsay_gui_glade-379330b0c46de9cf.d
@@ -0,0 +1,5 @@
+/home/nt2311-vn/Projects/Rust/rust_projects/catsay-gui-glade/target/debug/deps/libcatsay_gui_glade-379330b0c46de9cf.rmeta: src/main.rs
+
+/home/nt2311-vn/Projects/Rust/rust_projects/catsay-gui-glade/target/debug/deps/catsay_gui_glade-379330b0c46de9cf.d: src/main.rs
+
+src/main.rs:
---
+++ catsay-gui-glade/target/debug/deps/catsay_gui_glade-6745c9c50db4a463.d
@@ -0,0 +1,5 @@
+/home/nt2311-vn/Projects/Rust/rust_projects/catsay-gui-glade/target/debug/deps/libcatsay_gui_glade-6745c9c50db4a463.rmeta: src/main.rs
+
+/home/nt2311-vn/Projects/Rust/rust_projects/catsay-gui-glade/target/debug/deps/catsay_gui_glade-6745c9c50db4a463.d: src/main.rs
+
+src/main.rs:
