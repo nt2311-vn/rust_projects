da86cde9fadf48df149073816d2421f5623d92cb
nt2311-vn <nguyentoan231196@gmail.com>
2025-06-04 22:27:02 +0700

    move ownership of image output inside closer because we dont know when the closure is triggered and clone the pointer to perform a not costly copy

--- catsay-gui-glade/src/main.rs
+++ catsay-gui-glade/src/main.rs
@@ -1,4 +1,4 @@
-use gtk::prelude::*;
+use gtk::{glib::clone, prelude::*};
 
 fn main() {
     let application = gtk::Application::new(Some("nt2311-vn.catsay-gui-glade"), Default::default());
@@ -13,5 +13,17 @@ fn build_ui(app: &gtk::Application) {
 
     let window: gtk::Window = builder.object("applicationwindow1").unwrap();
     window.set_application(Some(app));
+
+    let message_input: gtk::Entry = builder.object("message_input").unwrap();
+    let button: gtk::Button = builder.object("generate_btn").unwrap();
+    let message_output: gtk::Label = builder.object("message_output").unwrap();
+    let image_output: gtk::Image = builder.object("image_output").unwrap();
+    let image_output_clone = image_output.clone();
+
+    button.connect_clicked(move |_| {
+        message_output.set_text(&format!("{}\n \\n \\", message_input.text().as_str()));
+        image_output_clone.show();
+    });
     window.show_all();
+    image_output.hide();
 }
