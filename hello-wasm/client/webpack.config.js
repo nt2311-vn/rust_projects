103c7ee277fb09b0416a4fb180ddfbbbe762ca99
nt2311-vn <nguyentoan231196@gmail.com>
2025-06-05 22:24:13 +0700

    build project with wasm complete

--- hello-wasm/client/webpack.config.js
+++ hello-wasm/client/webpack.config.js
@@ -1,14 +1,26 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: "./bootstrap.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bootstrap.js",
-  },
-  mode: "development",
-  plugins: [
-    new CopyWebpackPlugin(['index.html'])
-  ],
+	entry: "./bootstrap.js",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "bootstrap.js",
+	},
+	experiments: {
+		asyncWebAssembly: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.wasm$/,
+				type: "webassembly/async",
+			},
+		],
+	},
+	devServer: {
+		port: 8080,
+	},
+	mode: "development",
+	plugins: [new CopyWebpackPlugin(["index.html"])],
 };
