3f437b7e55b8bbf8a9346b18b0162e3b0b798959
nt2311-vn <nguyentoan231196@gmail.com>
2025-06-06 22:13:09 +0700

    write function to shrink image by half

--- wasm-image-processing/src/lib.rs
+++ wasm-image-processing/src/lib.rs
@@ -1,5 +1,8 @@
 mod utils;
 
+use image::imageops;
+use image::ImageBuffer;
+use image::RgbaImage;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -8,6 +11,11 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm-image-processing!");
+pub fn shrink_by_half(original_image: Vec<u8>, width: u32, height: u32) -> Vec<u8> {
+    let image: RgbaImage = ImageBuffer::from_vec(width, height, original_image).unwrap();
+
+    let output_image =
+        imageops::resize(&image, width / 2, height / 2, imageops::FilterType::Nearest);
+
+    output_image.into_vec()
 }
