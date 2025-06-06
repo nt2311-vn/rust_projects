81b8de42744db6c1d4c8833cba558e2ce0c73608
nt2311-vn <nguyentoan231196@gmail.com>
2025-06-06 22:07:08 +0700

    update html to upload and shrink file write javascript function to create url object and display image

--- wasm-image-processing/client/index.js
+++ wasm-image-processing/client/index.js
@@ -1,3 +1,29 @@
 import * as wasm from "hello-wasm-pack";
 
-wasm.greet();
+function setup(event) {
+	const fileInput = document.getElementById("image-upload");
+
+	fileInput.addEventListener("change", function (event) {
+		const file = event.target.files[0];
+		const imageUrl = window.URL.createObjectURL(file);
+
+		const image = new Image();
+		image.src = imageUrl;
+
+		image.addEventListener("load", (loadEvent) => {
+			const canvas = document.getElementById("preview");
+			canvas.width = image.naturalWidth;
+			canvas.height = image.naturalHeight;
+
+			canvas
+				.getContext("2d")
+				.drawImage(image, 0, 0, canvas.width, canvas.height);
+		});
+	});
+}
+
+if (document.readyState !== "loading") {
+	setup();
+} else {
+	window.addEventListener("DOMContentLoaded", setup);
+}
